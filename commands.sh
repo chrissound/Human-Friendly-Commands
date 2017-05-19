8627d11a14dfa9aedeff940c6314946d46ee73df
Chris Stryczynski <chris@soundshare.co.uk>
2017-05-19 19:31:04 +0100

    arch linux

--- commands.sh
+++ commands.sh
@@ -1,4 +1,18 @@
-stopAllDockerContainers() { sudo docker stop `sudo docker ps -q`}
-listRunningDockerContainers() { sudo docker ps}
 makeFilePermissionExecutable() { chmod +x $1 }
+
+#docker
+stopAllDockerContainers() { sudo docker stop `sudo docker ps -q`}
+deleteDockerComposeVolumes() { sudo docker-compose down -v }
+listRunningDockerContainers() { sudo docker ps}
+
+
+# git
 cdGitProjectRoot() { cd `git rev-parse --show-toplevel` }
+
+# arch linux pacman
+pacmanInstall () { sudo pacman -S $1}
+pacmanUpdate () { sudo pacman -Syu }
+
+# outputFiltering
+alias filterExclude="grep -v"
+alias filterOnly="grep"
