8d54a89facb4f42da9b5767ca0d590781707dcb1
Chris Stryczynski <chris@soundshare.co.uk>
2017-06-13 18:16:57 +0100

    new commands

--- commands.sh
+++ commands.sh
@@ -3,6 +3,14 @@ makeFilePermissionExecutable() { chmod +x $1 }
 makeOwnedByMyself() { sudo chown $USER:$USER -R $1 }
 makeFilePermissionEverything() { sudo chmod 777 -R $1 }
 
+#listFiles
+alias llSortBySizeAscend="ll -S -h"
+alias llSortBySizeDescend="ll -S -h -r"
+alias llSortByTime="ll -tr"
+
+#
+alias makeZip="zip -r $(basename $PWD) ."
+
 #search
 findByFilename() { find . -name "$1" }
 findByFilenameCaseInsensitive() { find . -iname "$1" }
@@ -11,6 +19,8 @@ findByFilenameCaseInsensitive() { find . -iname "$1" }
 stopAllDockerContainers() { sudo docker stop `sudo docker ps -q`}
 deleteDockerComposeVolumes() { sudo docker-compose down -v }
 listRunningDockerContainers() { sudo docker ps}
+listRunningDockerContainerIps() { sudo docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(sudo docker ps -q) }
+enterDockerContainerBash() { sudo docker exec -it $(sudo docker ps --format "{{.Names}}" | fzf | cat) /bin/bash }
 
 
 # git
@@ -20,6 +30,14 @@ cdGitProjectRoot() { cd `git rev-parse --show-toplevel` }
 pacmanInstall () { sudo pacman -S $1}
 pacmanUpdate () { sudo pacman -Syu }
 
+# systemd
+alias startService="sudo systemctl start "
+alias stopService="sudo systemctl stop "
+
+
 # outputFiltering
 alias filterExclude="grep -v"
 alias filterOnly="grep"
+
+# misc
+backup () { cp -r $1 $1.backup }
