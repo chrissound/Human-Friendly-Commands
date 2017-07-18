c730b21e698bc61b35d660b47c7c89df0b77b991
Chris Stryczynski <chris@soundshare.co.uk>
2017-07-18 22:27:16 +0100

    more commands. updated readme.

--- Readme.md
+++ Readme.md
@@ -1,9 +1,20 @@
 #Linux Command Lib
 
+##What problem does this solve?
+It's just a collection of verbose commands that abstract away all the more complicated commands.
+
+For example to zip the contents of the current directory it's just `makeZip` instead of `zip -r filename.zip .`, and `makeOwnedByMyself filename` instead of `sudo chown $USER:$USER -R filname`.
+
+If you use this with FZF, you only can fuzzily search for a command. For example `mz` for `MakeZip`.
+
+## Demo
 ![Alt text](input.gif?raw=true "Demonstration")
 
-Dependencies:
-fzf
+## Dependencies:
+- fzf
+
+## Installation: 
+Add `commands.sh` to your PATH. The functions and aliases will then be availble in your command prompt.
 
 ZSH keybindng (bind `cat ~/Projects/LinuxCommandLib/commands.txt | fzf --height 25%` to `CTRL-Z`):
 
--- commands.sh
+++ commands.sh
@@ -10,21 +10,30 @@ alias llSortByTime="ll -tr"
 
 #
 alias makeZip="zip -r $(basename $PWD) ."
+alias makeZipWithoutHiddenPaths="zip -r  $(basename $PWD) . -x '.*'"
 
 #search
 findByFilename() { find . -name "$1" }
 findByFilenameCaseInsensitive() { find . -iname "$1" }
 
-#docker
+#docker (requires docker, docker-compose)
 stopAllDockerContainers() { sudo docker stop `sudo docker ps -q`}
 deleteDockerComposeVolumes() { sudo docker-compose down -v }
+showLatestCreatedDockerContainer() { sudo docker ps -a --latest}
+getLatestCreatedDockerContainerId() { sudo docker ps -a --latest -q}
+startLatestCreatedDockerContainer() { sudo docker start `sudo docker ps -a --latest -q`}
+listDockerContainers() { sudo docker ps -a }
 listRunningDockerContainers() { sudo docker ps}
 listRunningDockerContainerIps() { sudo docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(sudo docker ps -q) }
 enterDockerContainerBash() { sudo docker exec -it $(sudo docker ps --format "{{.Names}}" | fzf | cat) /bin/bash }
+runCommandInDockerContainer() { sudo docker exec -it "$@"}
+runCommandInDockerContainerWithContainerPrompt() { sudo docker exec -it $(promptForRunningDockerContainer) "$@"}
+promptForRunningDockerContainer() { echo $(sudo docker ps --format "{{.Names}}" | fzf) }
 
 
 # git
 cdGitProjectRoot() { cd `git rev-parse --show-toplevel` }
+gitExportRepoAsZip() { git archive -o "$1" HEAD }
 
 # arch linux pacman
 pacmanInstall () { sudo pacman -S $1}
@@ -40,4 +49,7 @@ alias filterExclude="grep -v"
 alias filterOnly="grep"
 
 # misc
-backup () { cp -r $1 $1.backup }
+backup () { cp -r $1 $1.backup ; }
+copyPathToClipboard () { readlink -f $1 | xclip ; }
+# misc (requires xclip)
+copyFileToClipboard () { cat $1 | xclip ; }
